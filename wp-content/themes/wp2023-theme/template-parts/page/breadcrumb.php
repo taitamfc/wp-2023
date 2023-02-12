14ebb38ad2e6143b35a2a1611227799242efa42e
taitamfc <taitam8590@gmail.com>
2023-02-12 13:46:25 +0700

    Phan tach cac thanh phan giao dien

---
+++ wp-content/themes/wp2023-theme/template-parts/page/breadcrumb.php
@@ -0,0 +1,18 @@
+<?php global $theme_uri;?>
+<!-- Breadcrumb Section Begin -->
+<section class="breadcrumb-section set-bg" data-setbg="<?= $theme_uri; ?>/assets/img/breadcrumb.jpg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="breadcrumb__text">
+                    <h2>Blog</h2>
+                    <div class="breadcrumb__option">
+                        <a href="./index.html">Home</a>
+                        <span>Blog</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Breadcrumb Section End -->
