a5ef9d9161234cd21d262d900f898a88bbaef90d
taitamfc <taitam8590@gmail.com>
2023-02-12 13:46:25 +0700

    Phan tach cac thanh phan giao dien

---
+++ wp-content/themes/wp2023-theme/template-parts/footer/footer-bottom.php
@@ -0,0 +1,8 @@
+<div class="row">
+    <div class="col-lg-12">
+        <div class="footer__copyright">
+            <div class="footer__copyright__text"><p> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+            <div class="footer__copyright__payment"><img src="<?= $theme_uri; ?>/assets/img/payment-item.png" alt=""></div>
+        </div>
+    </div>
+</div>
